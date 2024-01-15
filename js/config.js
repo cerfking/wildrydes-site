d43bbc6536c715cab93c6ac0beec0696746e9417
Sebastian <huajinglu0312@gmail.com>
2024-01-15 13:39:40 -0800

    new_config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
   cognito: {
-    userPoolId: "us-east-1_MX1zjZbgi", // e.g. us-east-2_uXboG5pAb
-    userPoolClientId: "3jq7qohgj4kf8359ggj1pcvl46", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-    region: "us-east-1", // e.g. us-east-2
+    userPoolId: "us-east-2_h3Nn1YQvf", // e.g. us-east-2_uXboG5pAb
+    userPoolClientId: "1ffked74m3ajtlfiqn2rpit17p", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+    region: "us-east-2", // e.g. us-east-2
   },
   api: {
     invokeUrl: "https://grspkumd6j.execute-api.us-east-1.amazonaws.com/prod", // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
